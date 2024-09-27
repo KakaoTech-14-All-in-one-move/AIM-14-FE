9d150c65c48795c0cd66a1ad301bf548897246ce
teddy.kim <qazkyj0310@gmail.com>
2024-09-27 14:06:19 +0900

    AIM-18 add: sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { SidebarIcon } from './SidebarIcon';
+import { Divider } from './Divider';
+import { useServers } from '../../hooks/useServers';
+import { HomeIcon } from './icons/HomeIcon';
+
+const Sidebar: React.FC = () => {
+    const { servers, addServer } = useServers();
+
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                    bg-white dark:bg-gray-900 shadow-lg">
+            <SidebarIcon icon={<HomeIcon />} text="홈" />
+            <Divider />
+            {servers.map((server, index) => (
+                <SidebarIcon key={index} text={server} />
+            ))}
+            <Divider />
+            <SidebarIcon
+                icon={<span className="text-2xl">+</span>}
+                text="서버 추가"
+                onClick={addServer}
+            />
+        </div>
+    );
+};
+
+export default Sidebar;
