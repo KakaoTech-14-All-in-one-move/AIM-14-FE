70d3e2c4ac9d6b7f9031713a4a2b29910cf337fa
teddy.kim <qazkyj0310@gmail.com>
2024-09-29 13:10:38 +0900

    AIM-18 add: invite member button

--- src/components/Sidebar/ContextMenu.tsx
+++ src/components/Sidebar/ContextMenu.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Divider } from './Divider';
 
 interface ContextMenuProps {
     x: number;
     y: number;
+    onInvite: () => void;
     onRemove: () => void;
 }
 
-export const ContextMenu: React.FC<ContextMenuProps> = ({ x, y, onRemove }) => {
+export const ContextMenu: React.FC<ContextMenuProps> = ({ x, y, onInvite, onRemove }) => {
     return (
         <div
             className="absolute bg-black shadow-md rounded-md py-1 z-50"
@@ -16,6 +18,14 @@ export const ContextMenu: React.FC<ContextMenuProps> = ({ x, y, onRemove }) => {
                 minWidth: '120px'
             }}
         >
+            <button
+                className="block w-full text-left px-4 py-2 text-kakaoYellow hover:bg-gray-800 whitespace-nowrap"
+                onClick={onInvite}
+            >
+                멤버 초대하기
+            </button>
+            <Divider />
+            {/* TODO: 서버를 만든 관리자라면 채널, 카테고리를 관리할 수 있는 버튼 활성화 */}
             <button
                 className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-800 whitespace-nowrap"
                 onClick={onRemove}
