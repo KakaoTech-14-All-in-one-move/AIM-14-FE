3c36582c056ffc31f52424b6b8b3aff6c9628756
teddy.kim <qazkyj0310@gmail.com>
2024-10-14 17:13:15 +0900

    remove: dark mode setting

--- src/components/Home/Sidebar/index.tsx
+++ src/components/Home/Sidebar/index.tsx
@@ -10,7 +10,7 @@ const Sidebar: React.FC = () => {
   return (
     <div
       className="fixed top-0 left-0 h-screen w-16 flex flex-col
-                    bg-white dark:bg-discord900 shadow-lg"
+                    bg-discord900 shadow-lg"
     >
       <SidebarIcon icon={<HomeIcon />} text="홈" />
       <Divider />
