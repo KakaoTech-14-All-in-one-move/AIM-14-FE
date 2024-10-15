6d283e30f452c99e7abf61e189baabde26e895fc
teddy.kim <qazkyj0310@gmail.com>
2024-10-15 14:36:03 +0900

    add: channel bar

---
+++ src/components/Home/Channelbar/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Hash, Volume2, Monitor } from 'lucide-react';
+import ChannelList from '@/components/Home/Channelbar/ChannelList';
+import ChannelHeader from '@/components/Home/Channelbar/ChannelHeader';
+import ChannelFooter from '@/components/Home/Channelbar/ChannelFooter';
+import { ChannelProvider } from '@/components/Home/Channelbar/ChannelContext';
+
+const Channelbar: React.FC = () => {
+    return (
+        <ChannelProvider>
+            <div className="w-60 bg-discord800 text-white h-screen flex flex-col">
+                <ChannelHeader />
+                <div className="overflow-y-auto flex-grow px-2">
+                    <ChannelList type="text" icon={Hash} />
+                    <ChannelList type="voice" icon={Volume2} />
+                    <ChannelList type="video" icon={Monitor} />
+                </div>
+                <ChannelFooter />
+            </div>
+        </ChannelProvider>
+    );
+};
+
+export default Channelbar;
