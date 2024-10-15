66a8df6b8a4bf6c17750162e940384ab7e142c52
teddy.kim <qazkyj0310@gmail.com>
2024-10-15 14:37:11 +0900

    refactor: side bar setting

--- src/components/Home/Sidebar/index.tsx
+++ src/components/Home/Sidebar/index.tsx
@@ -8,10 +8,7 @@ const Sidebar: React.FC = () => {
   const { servers, addServer, removeServer } = useServerStore();
 
   return (
-    <div
-      className="fixed top-0 left-0 h-screen w-16 flex flex-col
-                    bg-discord900 shadow-lg"
-    >
+    <div className="h-screen w-16 flex flex-col bg-discord900 shadow-lg">
       <SidebarIcon icon={<HomeIcon />} text="홈" />
       <Divider />
       {servers.map((server, index) => (
