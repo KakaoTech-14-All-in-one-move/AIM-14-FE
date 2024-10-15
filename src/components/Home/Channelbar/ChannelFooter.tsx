d546dd596f1b370fe7b78da9e7463fe5b7438421
teddy.kim <qazkyj0310@gmail.com>
2024-10-15 14:36:49 +0900

    add: channel footer

---
+++ src/components/Home/Channelbar/ChannelFooter.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Settings } from 'lucide-react';
+import { useChannels } from '@/components/Home/Channelbar/ChannelContext';
+
+const ChannelFooter: React.FC = () => {
+    const { currentUser } = useChannels();
+
+    return (
+        <div className="p-4 border-t border-gray-700">
+            <div className="flex items-center justify-between text-gray-400">
+                <div className="flex items-center">
+                    <img src={currentUser.profileImage} alt={currentUser.nickname} className="w-8 h-8 rounded-full mr-2" />
+                    <span>{currentUser.nickname}</span>
+                </div>
+                <Settings size={18} className="cursor-pointer hover:text-gray-200" />
+            </div>
+        </div>
+    );
+};
+
+export default ChannelFooter;
