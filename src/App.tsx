dadfcd41842c310d5e5ecf2d7581338648d73d6f
Chanyoung Lee <96778638+hungrypro7@users.noreply.github.com>
2024-10-15 09:36:49 +0900

    Revert "Aim 36 implementing login UI ux"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from '@/components/Home';
-import Login from '@/components/Login';
-import { useStore } from '@/stores/login';
+import Sidebar from "./components/Sidebar";
 
-const App: React.FC = () => {
-  const { isLoggedIn } = useStore();
-
+function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-900">
-        <Routes>
-          <Route path="/login" element={isLoggedIn ? <Navigate to="/home" /> : <Login />} />
-          <Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+    <div className='flex'>
+      <Sidebar />
+    </div>
+  )
+}
 
 export default App;
--- src/components/Home/Sidebar/Divider.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Divider: React.FC = () => <hr className="sidebar-hr" />;
