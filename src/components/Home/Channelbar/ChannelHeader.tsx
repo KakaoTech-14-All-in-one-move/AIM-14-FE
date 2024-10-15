460488f691a01d855ac744480ecb95c31fd3189f
teddy.kim <qazkyj0310@gmail.com>
2024-10-15 14:46:40 +0900

    remove: border bottom

--- src/components/Home/Channelbar/ChannelHeader.tsx
+++ src/components/Home/Channelbar/ChannelHeader.tsx
@@ -3,7 +3,7 @@ import { ChevronDown } from 'lucide-react';
 
 const ChannelHeader: React.FC = () => {
     return (
-        <div className="flex items-center justify-between p-4 border-b border-gray-700">
+        <div className="flex items-center justify-between p-4 border-b border-black">
             <span className="font-semibold">KTB-14조</span>
             <ChevronDown size={20} />
         </div>
