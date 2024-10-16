97084b606768b711eea9f49b9b6343b842e3cd4f
teddy.kim <qazkyj0310@gmail.com>
2024-10-16 11:01:41 +0900

    add: online text

--- src/components/Home/Channelbar/ChannelFooter.tsx
+++ src/components/Home/Channelbar/ChannelFooter.tsx
@@ -6,13 +6,16 @@ const ChannelFooter: React.FC = () => {
     const { currentUser } = useChannels();
 
     return (
-        <div className="p-3">
+        <div className="p-3 bg-discord800">
             <div className="flex items-center justify-between text-gray-400">
                 <div className="flex items-center">
-                    <img src={currentUser.profileImage} alt={currentUser.nickname} className="w-8 h-8 rounded-full mr-2" />
-                    <span>{currentUser.nickname}</span>
+                    <img src={currentUser.profileImage} alt={currentUser.nickname} className="w-7 h-7 rounded-full mr-2" />
+                    <div className="flex flex-col leading-tight">
+                        <span className='text-sm font-bold leading-none'>{currentUser.nickname}</span>
+                        <span className='text-xs leading-none mt-px'>온라인</span>
+                    </div>
                 </div>
-                <Settings size={18} className="cursor-pointer hover:text-gray-200" />
+                <Settings size={21} className="cursor-pointer hover:text-gray-200" />
             </div>
         </div>
     );
