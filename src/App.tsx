f76d649d4564ee4794c53666b7c01e6cac05becc
neo <maiload77@gmail.com>
2024-10-18 23:19:52 +0900

    feat: Add RecordingPage Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
+<<<<<<< HEAD
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthEventHandler } from '@/components/Router/AuthEventHandler';
 import { AppRoutes } from '@/components/Router/AppRoutes';
 import { ToastProvider } from '@/components/Provider/ToastProvider';
+=======
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from '../src/components/Home';
+import Login from '../src/components/Login';
+import RecordingPage from '../src/page/RecordingPage';
+import { useStore } from './stores/login';
+>>>>>>> 3a29a03 (feat: Add RecordingPage Route)
 
 const App: React.FC = () => {
   return (
     <Router>
+<<<<<<< HEAD
       <ToastProvider>
         <AuthEventHandler />
         <AppRoutes />
       </ToastProvider>
+=======
+      <div className="min-h-screen bg-gray-900">
+        <Routes>
+          <Route path="/login" element={isLoggedIn ? <Navigate to="/home" /> : <Login />} />
+          <Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+          {/*<Route path="/feedback" element={isLoggedIn ? <RecordingPage /> : <Navigate to="/login" />} />*/}
+          <Route path="/feedback" element={<RecordingPage />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+>>>>>>> 3a29a03 (feat: Add RecordingPage Route)
     </Router>
   );
 };
