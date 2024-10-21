8a6b16404c11866ac77fb8476f79564239c9a6e5
neo <maiload77@gmail.com>
2024-10-21 09:23:32 +0900

    refactor: refactored by github comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { ToastProvider } from '@/components/Provider/ToastProvider';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from '../src/components/Home';
 import Login from '../src/components/Login';
-import RecordingPage from '../src/page/RecordingPage';
+import Index from './components/record';
 import { useStore } from './stores/login';
 >>>>>>> 3a29a03 (feat: Add RecordingPage Route)
 
@@ -25,8 +25,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/login" element={isLoggedIn ? <Navigate to="/home" /> : <Login />} />
           <Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
-          {/*<Route path="/feedback" element={isLoggedIn ? <RecordingPage /> : <Navigate to="/login" />} />*/}
-          <Route path="/feedback" element={<RecordingPage />} />
+          {/*<Route path="/feedback" element={isLoggedIn ? <Index /> : <Navigate to="/login" />} />*/}
+          <Route path="/feedback" element={<Index />} />
           <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </div>
