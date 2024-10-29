d5583c9fc944875b0588dbaff995a7d275cf64ff
teddy.kim <qazkyj0310@gmail.com>
2024-10-29 11:34:36 +0900

    AIM-51 refactor: use root path @

--- src/components/Router/AppRoutes.tsx
+++ src/components/Router/AppRoutes.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '@/components/Home';
 import Login from '@/components/Login';
-import { ProtectedRoute } from './ProtectedRoute';
-import { PublicRoute } from './PublicRoute';
+import { ProtectedRoute } from '@/components/router/ProtectedRoute';
+import { PublicRoute } from '@/components/router/PublicRoute';
+import { AuthEventHandler } from '@/components/router/AuthEventHandler';
 
 export const AppRoutes: React.FC = () => {
     return (
-
         <div className="min-h-screen bg-discord900">
+            <AuthEventHandler />
             <Routes>
                 <Route
                     path="/login"
