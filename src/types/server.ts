a6eff000046766b6d144c46ad4d5dde8a3c4fd9b
teddy.kim <qazkyj0310@gmail.com>
2024-11-21 17:33:36 +0900

    AIM-141 add: channel type

--- src/types/server.ts
+++ src/types/server.ts
@@ -3,4 +3,13 @@ export interface Server {
   server_name: string;
   server_image?: string;
   created_at?: string;
+  channels?: Channel[];
+}
+
+export interface Channel {
+  channelId: number;
+  serverId: number;
+  channelName: string;
+  channelCategory: 'CHAT' | 'VOICE' | 'VIDEO'; // 리터럴 타입으로 정의
+  channelPosition: number;
 }
