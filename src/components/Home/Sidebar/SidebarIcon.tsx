4c9e48eae8cc9865a04bd5920606505b22b3646a
teddy.kim <qazkyj0310@gmail.com>
2024-11-21 10:02:54 +0900

    AIM-140 refactor: same shape selected, is selected

--- src/components/Home/Sidebar/SidebarIcon.tsx
+++ src/components/Home/Sidebar/SidebarIcon.tsx
@@ -136,8 +136,9 @@ export const SidebarIcon: React.FC<SidebarIconProps> = ({
 
       <div
         className={`sidebar-icon group flex items-center justify-center w-12 h-12 mx-auto my-2 cursor-pointer hover:bg-discord700 
-              ${isSelected || isHovered ? 'rounded-2xl' : 'rounded-full'} 
-              transition-all duration-150 ease-in`}
+        ${isSelected ? 'bg-discord700' : ''} 
+        ${isSelected || isHovered ? 'rounded-2xl' : 'rounded-full'} 
+        transition-all duration-150 ease-in`}
         onClick={onClick}
         onContextMenu={handleContextMenu}
       >
