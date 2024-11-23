599d5d389932fd3c5dc73ab2a797e96526e0b2e3
neo <maiload77@gmail.com>
2024-11-23 13:12:08 +0900

    fix: Change serverId to Number

--- src/services/call/constants.ts
+++ src/services/call/constants.ts
@@ -1,25 +1,7 @@
-import { ChannelMapping } from '@/services/call/types.ts';
-
 export const CALL_API = {
   GET_WEBSOCKET_URL: '/api/v1/call',
-  DEFAULT_SERVER_ID: '12345',
-  DEFAULT_CHANNEL_ID: '5143992e-9dcd-45fe-bcc7-e337417b0cfe'
-};
-
-// 기존 음성 채널 매핑은 그대로 유지
-export const TEMP_CHANNEL_MAPPING: ChannelMapping = {
-  serverId: '12345',
-  channelName: '일반',
-  channelId: '5143992e-9dcd-45fe-bcc7-e337417b0cfe',
-  channelType: 'VOICE'
-};
-
-// 비디오 채널 매핑 추가
-export const VIDEO_CHANNEL_MAPPING: ChannelMapping = {
-  serverId: '12345',
-  channelName: '일반',
-  channelId: '6143992e-9dcd-45fe-bcc7-e337417b0cfe',
-  channelType: 'VIDEO'
+  DEFAULT_SERVER_ID: '1',
+  DEFAULT_CHANNEL_ID: '5143992e-9dcd-45fe-bcc7-e337417b0cfe',
 };
 
 export const OP_CODES = {
@@ -34,7 +16,7 @@ export const OP_CODES = {
   LEAVE_CHANNEL: 4,
   LEAVE_CHANNEL_ACK: 14,
   STATE_UPDATE: 5,
-  STATE_UPDATE_ACK: 15
+  STATE_UPDATE_ACK: 15,
 } as const;
 
 export const RECONNECT_DELAY = 5000;
