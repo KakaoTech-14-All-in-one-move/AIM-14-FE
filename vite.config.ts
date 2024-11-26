c28fa816ebb1ad4495ed4b17eab12f6c41c3b249
neo <maiload77@gmail.com>
2024-11-26 20:59:25 +0900

    chore: Add build setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 상대 경로 기반 빌드를 위해 추가
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html') // input 형식 수정
+    }
+  }
 });
