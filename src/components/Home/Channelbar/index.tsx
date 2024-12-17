64f31ce5681440eed6fccdb55fb247da591db70c
teddy.kim <qazkyj0310@gmail.com>
2024-12-17 13:11:31 +0900

    AIM-177 fix: setting

--- src/components/Home/Channelbar/index.tsx
+++ src/components/Home/Channelbar/index.tsx
@@ -15,7 +15,9 @@ const Channelbar: React.FC = () => {
           <ChannelList type="voice" icon={Volume2} />
           <ChannelList type="video" icon={Monitor} />
         </div>
-        <ChannelFooter className="bg-discord800" />
+        <div className='bg-discord800'>
+          <ChannelFooter />
+        </div>
       </div>
     </ChannelProvider>
   );
