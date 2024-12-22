122f63b6a877d8297085823a3fdb982b3bb13266
neo <maiload77@gmail.com>
2024-12-22 13:53:28 +0900

    hotFix: Change fields onIceCandidate to snake_case

--- src/services/call/webrtc/MediaServerConnection.ts
+++ src/services/call/webrtc/MediaServerConnection.ts
@@ -81,8 +81,8 @@ export class MediaServerConnection {
       if (event.candidate) {
         this.callConnection?.sendOp(OP_CODES.ON_ICE_CANDIDATE, {
           candidate: event.candidate.toJSON(),
-          sdpMid: event.candidate.sdpMid,
-          sdpMLineIndex: event.candidate.sdpMLineIndex
+          sdp_mid: event.candidate.sdpMid,
+          sdp_m_line_index: event.candidate.sdpMLineIndex
         });
       }
     };
